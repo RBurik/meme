1f5c5df8c715739324442a8b3bf5585f8b107c75
RBurik <42717498+RBurik@users.noreply.github.com>
2020-04-21 10:13:42 +0300

    Add files via upload

---
+++ mems/mem_modal.php
@@ -0,0 +1,55 @@
+<p class="linkify-marker" style="padding-top: 5px;">
+<img style='max-width:100%;max-height:100%;margin:10px;' src='/blog_html/index/promo/mems/photo_2020-04-18_00-30-27.jpg'>
+<br>
+Я готов БЕСПЛАТНО, в течении двух недель, заниматься продвижением Вашего сайта. Именно такой срок необходим что бы был виден результат. Дальше решение за Вами, остались довольны результатом, продолжаем сотрудничать. <br>
+<br>
+Анализ сайта и консультация бесплатно. <br>
++38 098 132 00 68 <br>
++38 063 132 00 68 (Viber, Telegram) <br>
+Skype: Игорь TRAFLINKS <br>
+<br>
+Сайты: <a href="https://traflinks.com" class="no-external-marker" target="_blank" rel="nofollow noopener">https://traflinks.com</a> и <a href="http://os.zp.ua" class="no-external-marker" target="_blank" rel="nofollow noopener">http://os.zp.ua</a> <br>
+Предоставлю полный пакет документов для бухгалтерии, по оказанным услугам <br>
+ </p>
+ 
+ <p><b>Код для вставки в HTML-виде</b></p>
+ <td><bdo dir="ltr"><textarea style="background-color: #FFFFFF; color: #999999;" id="code" cols="120" rows="10" readonly onclick="this.focus(); this.select();">
+ 
+ <p class="linkify-marker" style="padding-top: 5px;">
+<img style='max-width:100%;max-height:100%;margin:10px;' src='/blog_html/index/promo/mems/photo_2020-04-18_00-30-27.jpg'>
+<br>
+Я готов БЕСПЛАТНО, в течении двух недель, заниматься продвижением Вашего сайта. Именно такой срок необходим что бы был виден результат. Дальше решение за Вами, остались довольны результатом, продолжаем сотрудничать. <br>
+<br>
+Анализ сайта и консультация бесплатно. <br>
++38 098 132 00 68 <br>
++38 063 132 00 68 (Viber, Telegram) <br>
+Skype: Игорь TRAFLINKS <br>
+<br>
+Сайты: <a href="https://traflinks.com" class="no-external-marker" target="_blank" rel="nofollow noopener">https://traflinks.com</a> и <a href="http://os.zp.ua" class="no-external-marker" target="_blank" rel="nofollow noopener">http://os.zp.ua</a> <br>
+Предоставлю полный пакет документов для бухгалтерии, по оказанным услугам <br>
+ </p>
+ 
+ </textarea></bdo>
+ 
+ <p><b>Код для вставки в BB-коде</b></p>
+ <td><bdo dir="ltr"><textarea style="background-color: #FFFFFF; color: #999999;" id="code" cols="120" rows="10" readonly onclick="this.focus(); this.select();">
+ 
+[img]/blog_html/index/promo/mems/photo_2020-04-18_00-30-27.jpg[/img]
+[br]
+Я готов БЕСПЛАТНО, в течении двух недель, заниматься продвижением Вашего сайта. Именно такой срок необходим что бы был виден результат. Дальше решение за Вами, остались довольны результатом, продолжаем сотрудничать.
+[br][br]
+Анализ сайта и консультация бесплатно.
+[br]
++38 098 132 00 68
+[br]
++38 063 132 00 68 (Viber, Telegram)[br]
+Skype: Игорь TRAFLINKS
+[br]
+[br]
+Сайты: https://traflinks.com и http://os.zp.ua
+[br]
+Предоставлю полный пакет документов для бухгалтерии, по оказанным услугам <br>
+ 
+ 
+ </textarea></bdo>
+ 
